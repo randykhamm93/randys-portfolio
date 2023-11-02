38b3f7438229d8e26ed2e88e6a56b65d93e5af1f
Randy Hamm <randykhamm@gmail.com>
2023-11-02 14:11:01 -0500

    finished models and urls on backend and did create react app for front end

---
+++ frontend/src/components/Repairs.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import { Authorized } from "./views/Authorized"
+import { ApplicationViews } from "./views/ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import "./Repairs.css"
+
+
+export const Repairs = () => {
+	return <Routes>
+		<Route path="/about" element={<null />} />
+		<Route path="*" element={
+			<Authorized>
+				<>
+					<NavBar />
+					<ApplicationViews />
+				</>
+			</Authorized>
+
+		} />
+	</Routes>
+}
---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import { Repairs } from "./components/Repairs"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import { BrowserRouter } from "react-router-dom"
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(
+    <BrowserRouter>
+        <Repairs />
+    </BrowserRouter>
+)
+
