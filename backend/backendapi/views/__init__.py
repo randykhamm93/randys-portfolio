c0c2615598c458aa3e5b9c1a78b7733c107570d4
Randy Hamm <randykhamm@gmail.com>
2023-12-01 21:53:20 -0600

    Delete backend directory

--- backend/backendapi/views/__init__.py
+++
@@ -1,6 +0,0 @@
-from .about_view import AboutListView
-from .contact_view import ContactListView
-from .footer_view import FooterListView
-from .hero_view import HeroListView
-from .projects_view import ProjectListView
-from .technologies_view import TechnologyListView
